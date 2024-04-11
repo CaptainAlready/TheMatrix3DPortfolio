82d666b65c18f45669d2776c388afb02d3bd1b80
CaptainAlready <pkapetanidis@gmail.com>
2024-04-11 16:38:18 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { Canvas } from '@react-three/fiber'
+import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
+import { Html } from '@react-three/drei'
+import { Suspense, useState, useEffect } from 'react'
+import './styles.css'
+import { Scene } from './scene.jsx'
+import { LoadingScreen } from './LoadingScreen.js'
+
+const audio = new Audio("/cityrain.mp3");
+const isMobile = window.innerWidth < 1200;
+
+
+function toggleAudio() {
+  const audio_button = document.querySelector('.audio_button');
+  if (audio.paused) {
+    audio_button.value = "🔊";
+    audio.play();
+  } else {
+    audio_button.value = "🔇";
+    audio.pause();
+  }
+}
+
+
+export function App() {
+
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    if (start) {
+      audio.loop = true;
+      audio.volume = 0.2;
+      audio.play();
+    }
+  }, [start]);
+
+  return (
+    <>
+      <Canvas shadows camera={{ position: [0, 10, 150], fov: 30 }} >
+        <Suspense fallback={null}>
+          {start && <>
+            <Html center position-y={30}>
+              <div textalign='center' align='center'>
+                <h1 fontFamily='monospace' id={isMobile ? 'name_mobile' : 'name'}> Panos Kapetanidis</h1>
+                <p fontFamily='monospace' id={isMobile ? 'title_mobile' : 'title'}>Computer Engineer</p>
+                <input type='button' className="audio_button" value={"🔊"} onClick={toggleAudio}></input>
+              </div>
+            </Html>
+
+            <spotLight position={[0, 25, 0]} angle={0.3} penumbra={1} castShadow intensity={2.5} shadow-bias={-0.0001} />
+            <ambientLight intensity={0.8} color={0xe8dbf1} />
+
+
+            {isMobile ?
+              (<Scene center scale={window.innerWidth / 600} />)
+              : (<Scene position={[-2.5, -10, 0]} scale={window.innerWidth / 1800} />)
+            }
+
+            <Html center position-y={-30}>
+              <div className="instructions">
+                <h1 fontFamily='monospace' id={isMobile ? 'title_mobile' : 'title'}>
+                  Click on the desk to start
+                </h1>
+              </div>
+            </Html>
+
+            <EffectComposer>
+              <DepthOfField focusDistance={0} focalLength={0.7} bokehScale={4.5} height={480} />
+              <Bloom luminanceThreshold={0} luminanceSmoothing={0.3} height={300} />
+              <Noise opacity={0.15} />
+              <Vignette eskil={false} offset={0.1} darkness={1} />
+            </EffectComposer>
+          </>}
+        </Suspense>
+        <LoadingScreen started={start} onStarted={() => setStart(true)} />
+      </Canvas>
+
+    </>
+  );
+}
+
+
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import { App } from './App'
+import { MatrixBackground } from './matrix'
+
+function Overlay() {
+  return (
+    <div className='overlay' style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <div style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px' }}>
+        Panos
+        <br />
+        Kapetanidis
+      </div>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>TheMatrix<br />3DPortfolio </div>
+      <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>2024</div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  const isMobile = window.innerWidth < 1200;
+
+  createRoot(document.getElementById('root')).render(
+    <>
+      <MatrixBackground />
+      <Overlay />
+      <App />
+    </>
+  )
+
+  document.body.style.overflow = 'hidden'; // Disable scrolling
+
+}
+
+
+window.addEventListener('orientationchange', renderApp)
+renderApp()
+
+
